19d03de01fcd5a61a2ca2e47432e8d706ea69a2b
danie11z <daniel.estudando1@gmail.com>
2022-10-05 18:55:52 -0300

    Detalhes do responsivo da header

--- src/Styles/HeaderStyle.js
+++ src/Styles/HeaderStyle.js
@@ -16,6 +16,10 @@ export const MenuList = styled.ul`
     align-items: center;
     height: 9vh;
 
+    @media screen and (max-width: 820px) and (min-width: 700px){
+        width: 40vw;
+    }
+
     @media screen and (max-width: 700px) and (min-width: 501px){
         width: 50vw;
     }
@@ -30,9 +34,18 @@ export const MenuLink = styled(Link)`
     color: #696969;
     font-weight: 600;
     font-size: 1.3vw;
+    font-family: 'Roboto Condensed', sans-serif;
 
     &:hover{
         color: #000000;
+    }
+
+    @media screen and (max-width: 1440px) and (min-width: 820px){
+        font-size: 1.8vw;
+    }
+
+    @media screen and (max-width: 820px) and (min-width: 700px){
+        font-size: 2.5vw;
     }
 
     @media screen and (max-width: 700px) and (min-width: 500px){
