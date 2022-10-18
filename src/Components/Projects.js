bd552d923f1f490cfed7e703bb928905ed4fc035
danie11z <daniel.estudando1@gmail.com>
2022-10-18 07:05:18 -0300

    Início da estilização de Projetos

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,12 @@
 import React from "react"
+import * as S from "../Styles/ProjectsStyle"
 
 import Flavo from "../assets/flavo.png"
 import Portflow from "../assets/portflow.png"
 import Ods from "../assets/ods.png"
 import PP from "../assets/recipes.png"
+import Watchill from "../assets/watchill.png"
+import Pets from "../assets/pets.png"
 
 import GitLogo from "../assets/github.png"
 import VercelLogo from "../assets/logo-vercel.svg"
@@ -11,120 +14,120 @@ import VercelLogo from "../assets/logo-vercel.svg"
 export default function About(){
     return(
         <>
-            <section>
-                <h2>flavo</h2>
-                <div>
+            <S.Section>
+                <S.H2>flavo</S.H2>
+                <S.DivOne>
                     <div>
-                        <img src={Flavo} alt="Print do projeto" />
+                        <S.ImgOne src={Flavo} alt="Print do projeto" />
                         <div>
                             <a href="https://github.com/danie11z/flavo" title="Repositório" target="_blank">
-                                <img src={GitLogo} alt="Logo do Github" />
+                                <S.ImgTwo src={GitLogo} alt="Logo do Github" />
                             </a>
                             <a href="https://flavo.vercel.app/" title="Deploy" target="_blank">
-                                <img src={VercelLogo} alt="Logo do Vercel" />
+                                <S.ImgTwo src={VercelLogo} alt="Logo do Vercel" />
                             </a>
                         </div>
                     </div>
                     <div>
-                        <p>Projeto desenvolvido para telas mobile, em React.js com CSS. Ele simula a aba de um app de compras, possuindo um contador na parte de baixo, que quantifica os produtos a serem comprados.</p>
+                        <S.P>Projeto desenvolvido para telas mobile, em React.js com CSS. Ele simula a aba de um app de compras, possuindo um contador na parte de baixo, que quantifica os produtos a serem comprados.</S.P>
                     </div>
-                </div>
-            </section>
-            <section>
-                <h2>Portflow</h2>
-                <div>
+                </S.DivOne>
+            </S.Section>
+            <S.Section>
+                <S.H2>Portflow</S.H2>
+                <S.DivOne>
                     <div>
-                        <img src={Portflow} alt="Print do projeto" />
+                        <S.ImgOne src={Portflow} alt="Print do projeto" />
                         <div>
                             <a href="https://github.com/danie11z/Portflow" title="Repositório" target="_blank">
-                                <img src={GitLogo} alt="Logo do Github" />
+                                <S.ImgTwo src={GitLogo} alt="Logo do Github" />
                             </a>
                             <a href="https://danie11z.github.io/Portflow/" title="Deploy" target="_blank">
-                                <img src={VercelLogo} alt="Logo do Vercel" />
+                                <S.ImgTwo src={VercelLogo} alt="Logo do Vercel" />
                             </a>
                         </div>
                     </div>
                     <div>
-                        <p>Projeto desenvolvido em tela desktop e para tela de 375px, focado no HTML e no CSS. Ele simula uma loja de produtos.</p>
+                        <S.P>Projeto desenvolvido em tela desktop e para tela de 375px, focado no HTML e no CSS. Ele simula uma loja de produtos.</S.P>
                     </div>
-                </div>
-            </section>
-            <section>
-                <h2>ODS</h2>
-                <div>
+                </S.DivOne>
+            </S.Section>
+            <S.Section>
+                <S.H2>ODS</S.H2>
+                <S.DivOne>
                     <div>
-                        <img src={Ods} alt="Print do projeto" />
+                        <S.ImgOne src={Ods} alt="Print do projeto" />
                         <div>
                             <a href="https://github.com/danie11z/ods" title="Repositório" target="_blank">
-                                <img src={GitLogo} alt="Logo do Github" />
+                                <S.ImgTwo src={GitLogo} alt="Logo do Github" />
                             </a>
                             <a href="https://ods-navy.vercel.app/" title="Deploy" target="_blank">
-                                <img src={VercelLogo} alt="Logo do Vercel" />
+                                <S.ImgTwo src={VercelLogo} alt="Logo do Vercel" />
                             </a>
                         </div>
                     </div>
                     <div>
-                        <p>Projeto voltado para a pesquisa dos Objetivos de Desenvolvimento Sustentável e como eles se desdobram em meu bairro. Nele, eu utilizei react, aplicando styled-components e modal.</p>
+                        <S.P>Projeto voltado para a pesquisa dos Objetivos de Desenvolvimento Sustentável e como eles se desdobram em meu bairro. Nele, eu utilizei react, aplicando styled-components e modal.</S.P>
                     </div>
-                </div>
-            </section>
-            <section>
-                <h2>PairPrograming</h2>
-                <div>
+                </S.DivOne>
+            </S.Section>
+            <S.Section>
+                <S.H2>PairPrograming</S.H2>
+                <S.DivOne>
                     <div>
-                        <img src={PP} alt="Print do projeto" />
+                        <S.ImgOne src={PP} alt="Print do projeto" />
                         <div>
                             <a href="https://github.com/danie11z/PairPrograming" title="Repositório" target="_blank">
-                                <img src={GitLogo} alt="Logo do Github" />
+                                <S.ImgTwo src={GitLogo} alt="Logo do Github" />
                             </a>
                             <a href="https://pair-programing-5m2d.vercel.app/" title="Deploy" target="_blank">
-                                <img src={VercelLogo} alt="Logo do Vercel" />
+                                <S.ImgTwo src={VercelLogo} alt="Logo do Vercel" />
                             </a>
                         </div>
                     </div>
                     <div>
-                        <p>Projeto que foi feito para treinar o Gitflow. Nele, eu e minha dupla utilizamos o React com styled-components. Ele também é voltado para telas mobile e simula um site de receitas.</p>
+                        <S.P>Projeto que foi feito para treinar o Gitflow. Nele, eu e minha dupla utilizamos o React com styled-components. Ele também é voltado para telas mobile e simula um site de receitas.</S.P>
                     </div>
-                </div>
-            </section>
-            <section>
-                <h2>flavo</h2>
-                <div>
+                </S.DivOne>
+            </S.Section>
+            <S.Section>
+                <S.H2>Watchill</S.H2>
+                <S.DivOne>
                     <div>
-                        <img alt="Print do projeto" />
+                        <S.ImgOne src={Watchill} alt="Print do projeto" />
                         <div>
-                            <a href="#" title="Repositório" target="_blank">
-                                <img src={GitLogo} alt="Logo do Github" />
+                            <a href="https://github.com/danie11z/Watchill" title="Repositório" target="_blank">
+                                <S.ImgTwo src={GitLogo} alt="Logo do Github" />
                             </a>
-                            <a href="#" title="Deploy" target="_blank">
-                                <img src={VercelLogo} alt="Logo do Vercel" />
+                            <a href="https://watchill-531g.vercel.app/filmes" title="Deploy" target="_blank">
+                                <S.ImgTwo src={VercelLogo} alt="Logo do Vercel" />
                             </a>
                         </div>
                     </div>
                     <div>
-                        <p></p>
+                        <S.P>Projeto em que é simulada uma página de filmes. Nela, a API foi consumida via state. Também foi aplicado react-router-dom e filter no input para pesquisas.</S.P>
                     </div>
-                </div>
-            </section>
-            <section>
-                <h2>flavo</h2>
-                <div>
+                </S.DivOne>
+            </S.Section>
+            <S.Section>
+                <S.H2>Pets</S.H2>
+                <S.DivOne>
                     <div>
-                        <img alt="Print do projeto" />
+                        <S.ImgOne src={Pets} alt="Print do projeto" />
                         <div>
-                            <a href="#" title="Repositório" target="_blank">
-                                <img src={GitLogo} alt="Logo do Github" />
+                            <a href="https://github.com/danie11z/Pets" title="Repositório" target="_blank">
+                                <S.ImgTwo src={GitLogo} alt="Logo do Github" />
                             </a>
-                            <a href="#" title="Deploy" target="_blank">
-                                <img src={VercelLogo} alt="Logo do Vercel" />
+                            <a href="https://pets-three.vercel.app/" title="Deploy" target="_blank">
+                                <S.ImgTwo src={VercelLogo} alt="Logo do Vercel" />
                             </a>
                         </div>
                     </div>
                     <div>
-                        <p></p>
+                        <S.P>Página que simula um site de adoção de gatos e cachorros. Nele, a API foi consumida via useState. Também foi aplicado react-router-dom para construir as rotas.</S.P>
                     </div>
-                </div>
-            </section>
+                </S.DivOne>
+            </S.Section>
         </>
     )
 }
---
+++ src/Styles/ProjectsStyle.js
@@ -0,0 +1,25 @@
+import styled from "styled-components"
+
+export const Section = styled.section`
+    
+`
+
+export const H2 = styled.h2`
+    font-size: 2.7vw;
+`
+
+export const DivOne = styled.div`
+    display: flex;
+`
+
+export const ImgOne = styled.img`
+    width: 28.5vw;
+`
+
+export const ImgTwo = styled.img`
+    width: 4vw;
+`
+
+export const P = styled.p`
+    font-size: 1.5vw;
+`
